7a48435dd89b04363dbd865d236eec5bb79fec1b
julien44830 <deniaudjulien0@gmail.com>
2024-11-19 11:53:51 +0100

    remove coment

--- src/component/Couint.test.tsx
+++ src/component/Couint.test.tsx
@@ -5,42 +5,32 @@ import Count from "./Count";
 
 describe("Test de mon composant Count", () => {
     it("Should render the initial count", () => {
-        // Rendu du composant
         render(<Count />);
 
-        // Vérification du texte initial
         const button = screen.getByRole("button", { name: /count is 0/i });
         expect(button).toBeInTheDocument();
     });
 
     it("Should increment the count when the button is clicked", () => {
-        // Rendu du composant
         render(<Count />);
 
-        // Cible le bouton
         const button = screen.getByRole("button", { name: /count is 0/i });
 
-        // Simule un clic
         fireEvent.click(button);
 
-        // Vérifie que le texte est mis à jour
         expect(
             screen.getByRole("button", { name: /count is 1/i })
         ).toBeInTheDocument();
     });
 
     it("Should increment the count multiple times", () => {
-        // Rendu du composant
         render(<Count />);
 
-        // Cible le bouton
         const button = screen.getByRole("button", { name: /count is 0/i });
 
-        // Simule plusieurs clics
         fireEvent.click(button);
         fireEvent.click(button);
 
-        // Vérifie que le texte est mis à jour
         expect(
             screen.getByRole("button", { name: /count is 2/i })
         ).toBeInTheDocument();
